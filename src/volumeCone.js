f3835f5892596119613e999d532b91a7bea44805
Matsvei Niaverau <matvej2002@gmail.com>
2024-11-06 09:40:11 +0100

    volumeCone: Solve the task

--- src/volumeCone.js
+++ src/volumeCone.js
@@ -5,5 +5,8 @@
  * @returns {number}
  */
 module.exports.volumeCone = function volumeCone(h, r) {
+  // V=(1/3) * π * r² * h.
+  const Vformula = (1/3 * Math.PI * Math.pow(r, 2) * h);
+  return Vformula.toFixed(2)
   throw new Error('Not implemented');
 };

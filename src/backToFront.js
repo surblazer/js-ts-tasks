810c9a6b64015f516df7ad2a8933cd7309575fd7
Matsvei Niaverau <matvej2002@gmail.com>
2024-11-06 11:18:00 +0100

    backToFront: Solve the task

--- src/backToFront.js
+++ src/backToFront.js
@@ -5,5 +5,11 @@
  * @returns {string}
  */
 module.exports.backToFront = function backToFront(str, symbolsCount) {
+  if (symbolsCount > str.length) {
+    return str;
+  }
+
+  const wrapStr = str.slice(-symbolsCount);
+  return wrapStr + str + wrapStr;
   throw new Error('Not implemented');
 };

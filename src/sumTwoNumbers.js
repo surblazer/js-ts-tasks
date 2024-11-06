9955de03fa778fb32cae5c3dadc328b5a4cffd01
Matsvei Niaverau <matvej2002@gmail.com>
2024-11-06 09:40:01 +0100

    sumTwoNumbers: Solve the task

--- src/sumTwoNumbers.js
+++ src/sumTwoNumbers.js
@@ -5,5 +5,15 @@
  * @returns {number}
  */
 module.exports.sumTwoNumbers = function sumTwoNumbers(firstNumber, secondNumber) {
+  const parseNumber = (num) => {
+    if (typeof num === 'string') {
+      const trimmedstr = String(num).trim();
+      const finalnum = trimmedstr.split(' ').join('');
+      return parseInt(finalnum, 10);
+    }
+    return num;
+  };
+
+  return parseNumber(firstNumber) + parseNumber(secondNumber);
   throw new Error('Not implemented');
 };

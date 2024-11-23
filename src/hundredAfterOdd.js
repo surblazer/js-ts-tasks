102f3eef1650efb16cd1d168627ef2579047e713
Matsvei Niaverau <matvej2002@gmail.com>
2024-11-23 18:52:28 +0100

    hundredAfterOdd: Complete exercise

--- src/hundredAfterOdd.js
+++ src/hundredAfterOdd.js
@@ -4,5 +4,13 @@
  * @returns {Object}
  */
 module.exports.hundredAfterOdd = function hundredAfterOdd(arr) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  const hundredArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    hundredArray.push(arr[i]);
+    if (arr[i] % 2) {
+      hundredArray.push(100);
+    }
+  }
+  return hundredArray;
 };

0e4cefb1ac7b8c109f58891620ec92c23f6851b7
Matsvei Niaverau <matvej2002@gmail.com>
2024-11-23 19:34:43 +0100

    maxCommonSub: Complete exercise

--- src/maxCommonSub.js
+++ src/maxCommonSub.js
@@ -9,5 +9,15 @@
  * @returns {number}
  */
 module.exports.maxCommonSub = function maxCommonSub(str1, str2) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  let maxSubstring = '';
+
+  for (let i = 0; i < str1.length; i++) {
+    for (let j = i + 1; j <= str1.length; j++) {
+      const substring = str1.slice(i, j);
+      if (str2.includes(substring) && substring.length > maxSubstring.length) {
+        maxSubstring = substring;
+      }
+    }
+  }
+  return maxSubstring;
 };

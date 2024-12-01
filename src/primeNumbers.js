0e5b8110df7dcceb23fd4adc5cc417d8094b272d
Matsvei Niaverau <matvej2002@gmail.com>
2024-12-01 18:02:23 +0100

    primeNumbers: completed task

--- src/primeNumbers.js
+++ src/primeNumbers.js
@@ -4,5 +4,25 @@
  * @returns {function}
  */
 module.exports.primeNumbers = function primeNumbers(highestNumber) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function (num1, num2) {
+    if (num1 > num2) {
+      [num1, num2] = [num2, num1];
+    }
+
+    num1 = Math.max(2, num1);
+    num2 = Math.min(highestNumber, num2);
+
+    const primes = [];
+    for (let i = num1; i <= num2; i++) {
+      let isPrime = true;
+      for (let j = 2; j * j <= i; j++) {
+        if (i % j === 0) {
+          isPrime = false;
+          break;
+        }
+      }
+      if (isPrime) primes.push(i);
+    }
+    return primes;
+  };
 };

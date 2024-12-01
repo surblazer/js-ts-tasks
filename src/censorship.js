8b0a2b036a2afd7c956f1974e471189e8a5c4f3d
Matsvei Niaverau <matvej2002@gmail.com>
2024-12-01 17:44:52 +0100

    censorship: completed task

--- src/censorship.js
+++ src/censorship.js
@@ -14,5 +14,10 @@
  * @returns {function}
  */
 module.exports.censorship = function censorship(forbidden) {
-  throw new Error('Not implemented'); // remove me and write a solution
+  return function (replace) {
+    for (let element of forbidden) {
+      replace = replace.replaceAll(element, '*'.repeat(element.length));
+    }
+    return replace;
+  };
 };
